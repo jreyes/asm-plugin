9746a4bb5bef7e31ddb17332639bd89421a384d2
Johann Reyes <jreyes@vaporwarecorp.com>
2017-05-21 22:39:45 -0400

    feat($pipe): Added TruncateCharactersPipe

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { EventBus } from './src/event/event-bus';
+import { TruncateCharactersPipe } from './src/pipe/truncate-characters.pipe';
 import { AlertService } from './src/service/alert.service';
 import { DateUtils } from './src/service/date-util.service';
 import { EventManager } from './src/service/event-manager.service';
@@ -11,6 +12,7 @@ export * from './src/event/refresh-event';
 export * from './src/event/event-handler';
 export * from './src/event/handler-registration';
 export * from './src/event/event-bus';
+export * from './src/pipe/truncate-characters.pipe';
 export * from './src/pluginbus/plugin-bus';
 export * from './src/service/alert.service';
 export * from './src/service/date-util.service';
@@ -18,7 +20,14 @@ export * from './src/service/event-manager.service';
 export * from './src/service/proxy.service';
 export * from './src/widget/widget-view';
 
-@NgModule()
+@NgModule({
+  declarations: [
+    TruncateCharactersPipe
+  ],
+  exports: [
+    TruncateCharactersPipe
+  ]
+})
 export class MirrorPluginModule {
   public static forRoot(): ModuleWithProviders {
     return {
